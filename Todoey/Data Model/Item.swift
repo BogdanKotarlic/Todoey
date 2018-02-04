f9fec5d2dd7652bcd995cdd4e83c0b5b3249f8fa
BogdanKotarlic <kotarlicbogdan@gmail.com>
2018-02-04 11:00:58 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Bogdan Kotarlic on 2/4/18.
+//  Copyright © 2018 Bogdan Kotarlic. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
